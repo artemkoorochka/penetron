50fae3e0630a3fd74ad3a23fe8b67d6f73e58505
artemkoorochka <artem@koorochka.com>
2019-08-25 14:54:36 +0300

    BX useful

--- koorochka/local/js/main/core/index.php
+++ koorochka/local/js/main/core/index.php
@@ -5,12 +5,12 @@
 use Matrix\Main\Page\Asset;
 require($_SERVER['DOCUMENT_ROOT'].'/matrix/header.php');
 // BX test libruaries
-Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/local/js/main/core/core.js");
+Asset::getInstance()->addJs("/local/js/main/core/core.js");
 // Koorochka test scripts
-Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/koorochka/local/js/main/core/script.js");
+Asset::getInstance()->addJs("/koorochka/local/js/main/core/script.js");
 ?>
 
-    <div id="BX"></div>
+    <div id="BX">BX</div>
 
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/matrix/footer.php');
---
+++ koorochka/local/js/main/core/script.js
@@ -0,0 +1,13 @@
+/**
+ * Debug tools
+ * @param value
+ */
+function d(value) {
+    console.info(value);
+}
+
+
+var test = BX("BX");
+test.innerHTML = "Every day";
+
+    
