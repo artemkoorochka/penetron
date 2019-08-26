0a62ca359ef5bab98e94b53d7966b2b9353ef91b
artemkoorochka <artem@koorochka.com>
2019-08-26 18:48:48 +0300

    Test DB variable

--- activity/koorochka/activity/cognitive/focus/module/main/db/table.list.php
+++ activity/koorochka/activity/cognitive/focus/module/main/db/table.list.php
@@ -1,18 +1,27 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/matrix/modules/main/include/prolog_before.php");
+require($_SERVER['DOCUMENT_ROOT'] . "/matrix/header.php");
+global $DB;
 
 
 $arParams = array(
     "MODULE" => "iblock"
 );
 
+
 $arResult = array(
-    "DB",
-    "ITEMS"
+    "DB" => false,
+    "NEO" => false,
+    "ITEMS" => false
 );
 
-d($arParams);
+$arResult["DB"] = $DB->Query("select ID, NAME FROM b_user");
 
-d($arResult);
+while ($user = $arResult["DB"]->Fetch())
+{
+    $arResult["ITEMS"][] = $user;
+}
 
+d($arResult["ITEMS"]);
+
+require($_SERVER['DOCUMENT_ROOT'] . "/matrix/footer.php");
 ?>
